7dfbd5fbdc6e4e981adeb9b037559e0e1ba648b1
mi <musailliasu7@gmail.com>
2023-03-06 10:06:58 +0100

    folder structure setup

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "../views/home/Home";
+
+// Style
+import "../index.css";
+
+const App = () => {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  )
+
+}
+
+export default App;
